08da318b11a0a0a382f177a4dfe055d871829bbe
parag-ingalkar <paragingalkar@gmail.com>
2025-05-05 14:50:18 -0400

    Implemented custom fetch hook

--- src/Components/FetchYogaData.jsx
+++ src/Components/FetchYogaData.jsx
@@ -2,26 +2,25 @@ import React from 'react'
 import useFetch from './UseFetch'
 import './FetchData.css'
 
-const FetchData = () => {
-    const [data]=useFetch('https://api.npoint.io/9045c260b1565daa9e15');
+const FetchYogaData = () => {
+    const [data]=useFetch('https://api.npoint.io/4459a9a10e43812e1152');
     console.log(data);
-  return (
-    <>
-     <ul className='list_data_main'>
+    return (
+        <>
+        <ul className='list_data_main'>
         <h1 className='usefetch_heading'>Use Fetch Custom Hook</h1>
         {data && data.map((e, index)=>(
             <>
              <li key={index} className='list_data'>
             <h3>{e.name}</h3>
-            <p><strong>Importance: </strong>{e.importance}</p>
             <p><strong>Benefits: </strong>{e.benefits}</p>
-            <p><strong>Time to eat: </strong>{e.best_time_to_intake}</p>
+            <p><strong>Time Duration: </strong>{e.time_duration}</p>
             </li>
             </>
         ))}
      </ul>
-    </>
-  )
+        </>
+    )
 }
 
-export default FetchData
+export default FetchYogaData
